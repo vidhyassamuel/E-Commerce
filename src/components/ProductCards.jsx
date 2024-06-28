6eaf33334ec44f3ef6b1f317b589d98bb6284c8a
Prabhatlodhi <lodhi.prabhat@gmail.com>
2024-06-28 20:15:39 +0530

    search bar added

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -5,12 +5,15 @@ import "../css/ProductCards.css";
 
 const ProductCards = () => {
   const [products, setProducts] = useState([]);
+  const [searchText, setSearchText] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
     axios
       .get(PRODUCT_API)
       .then((response) => {
         setProducts(response?.data);
+        setFilteredProducts(response?.data);
         console.log(response.data, "<==response.data");
       })
       .catch((error) => {
@@ -20,21 +23,59 @@ const ProductCards = () => {
 
   const imageSize = { width: "150px", height: "150px" };
 
+  const handleSearch = () => {
+    const filteredData = products.filter((product) =>
+      product.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredProducts(filteredData);
+    console.log("filteredData", filteredData);
+  };
+
   return (
-    <div className="cardWrapper">
-      {products?.map((product, index) => {
-        return (
-          <div key={index} className="mainCard">
-            <img src={product.image} alt={product.title} style={imageSize} />
-            <h6> {product.title} </h6>
-            <h4> {product?.price} </h4>
-            <div>
-              <span> {product?.rating.rate} </span>
+    <>
+      <input
+        type="text"
+        className="search-input"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div className="cardWrapper">
+        {filteredProducts.length === 0 ? (
+          <h1>No Products found</h1>
+        ) : (
+          filteredProducts?.map((product, index) => {
+            return (
+              <div key={index} className="mainCard">
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  style={imageSize}
+                />
+                <h6> {product.title} </h6>
+                <h4> {product?.price} </h4>
+                <div>
+                  <span> {product?.rating.rate} </span>
+                </div>
+              </div>
+            );
+          })
+        )}
+
+        {/* {products?.map((product, index) => {
+          return (
+            <div key={index} className="mainCard">
+              <img src={product.image} alt={product.title} style={imageSize} />
+              <h6> {product.title} </h6>
+              <h4> {product?.price} </h4>
+              <div>
+                <span> {product?.rating.rate} </span>
+              </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })} */}
+      </div>
+    </>
   );
 };
 
