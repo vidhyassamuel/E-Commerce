a41438e833d57ec3ac18ea3fa6f5c3ce3cd3d2ab
Prabhatlodhi <lodhi.prabhat@gmail.com>
2024-07-01 17:53:28 +0530

    details page added

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,25 +1,41 @@
 import { useEffect, useState } from "react";
-
+import { PRODUCT_API } from "../constant/apiConstant";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const DetailsPage = () => {
+  const [product, setProduct] = useState([]);
+  const { id } = useParams();
+ 
 
-    useEffect(() => {
-        fetchSingleData();
-      }, []);
-    
-      const fetchSingleData = async () => {
-        try {
-          const response = await axios.get(PRODUCT_API+id);
-        //   setProducts(response.data);
-        //   setFilteredProducts(response.data);
-        } catch (error) {
-          console.error(`Error in fetching product list`, error);
-        }
-      };
+  useEffect(() => {
+    fetchSingleData(id);
+  }, []);
+
+  const fetchSingleData = async (id) => {
+    try {
+      const response = await axios.get(`${PRODUCT_API}/${id}`); 
+      setProduct(response?.data);
+    } catch (error) {
+      console.error(`Error in fetching single product`, error);
+    }
+  };
 
   return (
-    <div>DetailsPage</div>
-  )
-}
+    <div className="details-container">
+      <h2>Product Details</h2>
+      {product ? (
+        <div>
+          <img src={product.image} alt={product.title} />
+          <h3> {product.title} </h3>
+          <p> {product.description} </p>
+          <p> {product.price} </p>
+        </div>
+      ) : (
+        <h1>Loading product details...</h1>
+      )}
+    </div>
+  );
+};
 
-export default DetailsPage
+export default DetailsPage;
